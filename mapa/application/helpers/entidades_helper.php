26592c06e6dd9c608eb1a00cf63f35d88c7f41ab
jhonniperdomo <62190189+jhonniperdomo@users.noreply.github.com>
2020-04-03 11:09:12 -0300

    mapa

---
+++ mapa/application/helpers/entidades_helper.php
@@ -0,0 +1,16 @@
+<?php
+class wan_map{
+
+  public $id;
+  public $Cedula;
+  public $Nombre;
+  public $Apellido;
+  public $Nacimiento;
+  public $Pais;
+  public $Ciudad;
+  public $latitud;
+    public $logitud;
+    public $Contagio;
+    public $Comentario;
+
+}
